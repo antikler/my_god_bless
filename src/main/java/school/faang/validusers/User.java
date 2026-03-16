7e62d443d6894794c21b11157f15adc40fcb16b9
Ievlev Rostislav <rostic2004@gmail.com>
2026-03-16 20:29:32 +0300

    Gerold_1_2_2 final_fix

--- src/main/java/school/faang/validusers/User.java
+++ src/main/java/school/faang/validusers/User.java
@@ -2,38 +2,38 @@ package school.faang.validusers;
 
 import lombok.Data;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 
 @Data
 public class User {
-    private static final List<String> VALID_JOBS = Arrays.asList("Google", "Uber", "Amazon");
-    private static final List<String> VALID_ADDRESSES = Arrays.asList("London", "New York", "Amsterdam");
+    private static final Integer VALID_AGE = 18;
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private String name;
     private Integer age;
     private String job;
     private String address;
 
     public User(String name, Integer age, String job, String address) {
-        if (!name.isEmpty()) {
-            if (age >= 18) {
-                if (VALID_JOBS.contains(job)) {
-                    if (VALID_ADDRESSES.contains(address)) {
-                        this.name = name;
-                        this.age = age;
-                        this.job = job;
-                        this.address = address;
-                    } else {
-                        throw new IllegalArgumentException("Not valid address");
-                    }
-                } else {
-                    throw new IllegalArgumentException("Not valid job");
-                }
-            } else {
-                throw new IllegalArgumentException("Not valid age");
-            }
-        } else {
+        validUser(name, age, job, address);
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void validUser(String name, Integer age, String job, String address) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("Name is empty");
+        }
+        if (age < VALID_AGE) {
+            throw new IllegalArgumentException("Not valid age");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Not valid job");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Not valid address");
         }
     }
 }
