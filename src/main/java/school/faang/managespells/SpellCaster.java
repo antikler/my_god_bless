3dc6a919f9d847b5593406b977679c6f736c895d
Ievlev Rostislav <rostic2004@gmail.com>
2026-03-22 20:42:39 +0300

    Gerold_2_1_2 fix_valid

--- src/main/java/school/faang/managespells/SpellCaster.java
+++ src/main/java/school/faang/managespells/SpellCaster.java
@@ -2,6 +2,9 @@ package school.faang.managespells;
 
 public class SpellCaster {
     public void cast(String spell, SpellAction spellAction) {
+        if (spell.isBlank()) {
+            throw new IllegalArgumentException("Name is empty or contains only spaces!");
+        }
         System.out.println(spellAction.act(spell));
     }
 }
